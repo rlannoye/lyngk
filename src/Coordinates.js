9b2763eae285debaa6431c581cdb03c7b83695c1
rlannoye <rem.lannoye@gmail.com>
2017-10-02 11:20:10 +0200

    Code qui permet de passer le test de l'histoire 1

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -1,4 +1,13 @@
 "use strict";
 
 Lyngk.Coordinates = function (c, l) {
+    var colonne=c;
+    var ligne=l;
+
+    this.est_valid=function(){
+        if(colonne==="A" && ligne===1){
+            return false;
+        }
+    }
+
 };
