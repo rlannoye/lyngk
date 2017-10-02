1a018994cd157a0c81935b0aa0803f8b7a117de9
rlannoye <rem.lannoye@gmail.com>
2017-10-02 10:42:46 +0200

    Test de l'histoire 1

--- test/test.js
+++ test/test.js
@@ -1,3 +1,8 @@
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+LyngkTestCase.prototype.test1=function(){
+    var c=new Lyngk.Coordinates("A",1);
+    assertTrue(c.est_valid()==false);
+};
