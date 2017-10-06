2c8792921b8899ecfba7c380d5de640962421eac
rlannoye <rem.lannoye@gmail.com>
2017-10-06 11:13:05 +0200

    Test de l'histoire 2

--- test/test.js
+++ test/test.js
@@ -6,3 +6,19 @@ LyngkTestCase.prototype.test1=function(){
     var c=new Lyngk.Coordinates("A",1);
     assertTrue(c.est_valid()==false);
 };
+
+LyngkTestCase.prototype.test2=function(){
+    var colonnes="ABCDEFGHI";
+    var cpt=0;
+    var c;
+    for(var i=0;i<9;i++) {
+        for (var j = 0; j < 9; j++) {
+            var c = new Lyngk.Coordinates(colonnes[i], j + 1);
+            if (c.danstableau()) {
+                cpt++;
+            }
+        }
+    }
+    assertTrue(cpt===43);
+
+};
