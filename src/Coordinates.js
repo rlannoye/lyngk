84e31fc3147814d9438de137a8535a131c98ed58
rlannoye <rem.lannoye@gmail.com>
2017-10-06 11:14:18 +0200

    Code qui permet de passer le test de l'histoire 2

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -10,4 +10,23 @@ Lyngk.Coordinates = function (c, l) {
         }
     }
 
+    this.danstableau=function(){
+        var tabval={
+            "A" : [3 ,3],
+            "B" : [2 ,5],
+            "C" : [1 ,7],
+            "D" : [2 ,7],
+            "E" : [2 ,8],
+            "F" : [3 ,8],
+            "G" : [3 ,9],
+            "H" : [5 ,8],
+            "I" : [7 ,7]
+        };
+        if(l>=tabval[c][0] && l<=tabval[c][1]){
+            return true;
+        }
+        return false;
+    }
+
 };
+
