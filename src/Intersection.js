92972e2487a85d2a295d4946146bc259c087df47
rlannoye <rem.lannoye@gmail.com>
2017-10-09 11:46:40 +0200

    Code valide de l'histoire 8

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,14 +2,21 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c) {
-};
-
 Lyngk.Intersection=function(){
     var etat=Lyngk.State.VACANT;
+    var color;
 
     this.getEtat=function(){
         return etat;
+    }
+
+    this.getColor=function(){
+        return color;
+    }
+
+    this.pion=function(c){
+        etat=Lyngk.State.ONE_PIECE;
+        color=c;
     }
 
 
