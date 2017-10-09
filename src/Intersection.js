4c26f757fda4b2ba1750ee9cd8bb6e51d3acf7a8
rlannoye <rem.lannoye@gmail.com>
2017-10-09 11:25:45 +0200

    Code histoire 7 valide

--- src/Intersection.js
+++ src/Intersection.js
@@ -4,3 +4,13 @@ Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function (c) {
 };
+
+Lyngk.Intersection=function(){
+    var etat=Lyngk.State.VACANT;
+
+    this.getEtat=function(){
+        return etat;
+    }
+
+
+};
