de62aa140c86bdc624efd2bd05af8616e7f0add5
rlannoye <rem.lannoye@gmail.com>
2017-10-13 10:36:46 +0200

    Test de l'histoire 7 revu

--- test/test.js
+++ test/test.js
@@ -28,13 +28,11 @@ LyngkTestCase.prototype.test3=function(){
     var l=3;
     var ch=c+l;
     var c=new Lyngk.Coordinates(c,l);
-    console.log(ch);
     assertTrue(c.toString()==ch);
 };
 
 LyngkTestCase.prototype.test4=function(){
     var c =new Lyngk.Coordinates("A",1);
-    console.log(c);
     assertTrue(c.toString()==="invalid");
 };
 
@@ -51,11 +49,23 @@ LyngkTestCase.prototype.test6hash=function(){
 };
 
 LyngkTestCase.prototype.test7etat=function(){
-    var c = new Lyngk.Intersection();
-    assertTrue(c.getEtat() === Lyngk.State.VACANT);
+    var lettres="ABCDEFGHI";
+    var compteur=0;
+    for(var i=0;i<lettres.length;i++){
+        for(var j=1;j<=9;j++){
+            var C=new Lyngk.Coordinates(lettres[i],j);
+            if(C.est_valid()){
+                var intersec=new Lyngk.Intersection(C);
+                if(intersec.getState() !==0){
+                    compteur++;
+                }
+            }
+        }
+    }
+    assertTrue(compteur===0);
 };
 
-LyngkTestCase.prototype.test8color=function(){
+/*LyngkTestCase.prototype.test8color=function(){
     var c = new Lyngk.Intersection();
     c.pion(Lyngk.Color.BLUE);
     assertTrue(c.getEtat()===Lyngk.State.ONE_PIECE && c.getColor()===Lyngk.Color.BLUE);
@@ -68,3 +78,14 @@ LyngkTestCase.prototype.test9color=function(){
     assertTrue(c.getEtat()===Lyngk.State.STACK && c.getColor()===Lyngk.Color.RED);
 
 };
+*/
+//LyngkTestCase.prototype.test10color=function(){
+  //  var c = new Lyngk.Intersection();
+    //c.pion(Lyngk.Color.BLUE);
+    //c.pion(Lyngk.Color.RED);
+    //c.pion(Lyngk.Color.BLACK);
+    //c.pion(Lyngk.Color.GREEN);
+    //c.pion(Lyngk.Color.IVORY);
+    //assertTrue(c.getEtat()===Lyngk.State.FULL_STACK && c.getColor()===Lyngk.Color.IVORY);
+//};
+
